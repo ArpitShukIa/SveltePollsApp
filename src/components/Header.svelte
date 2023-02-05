0c2c81d807f654ad80c8a37701997f897eb65c3c
Arpit Shukla <arpitshukla62@gmail.com>
2023-02-05 20:13:13 +0530

    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+    <h1>
+        <img alt="Poll Ninja Logo" src="src/assets/poll_ninja_logo.svg">
+    </h1>
+</header>
+
+<style>
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+
+    img {
+        max-width: 180px;
+    }
+</style>
